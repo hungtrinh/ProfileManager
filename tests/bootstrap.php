e5f0de694f3fe209b40b2eb0ef30e8fa3b8f3147
Hung Trinh <trinhduchung@gmail.com>
2015-07-09 13:37:10 +0700

    fix autoload zend framework with phpunit 3.7.*

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,9 +11,11 @@ defined('APPLICATION_ENV')
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/../vendor/zendframework/zendframework1/library'),
     get_include_path(),
 )));
 
-require_once "vendor/autoload.php";
+require_once realpath(APPLICATION_PATH . '/../vendor/autoload.php');
+
 require_once 'Zend/Loader/Autoloader.php';
 Zend_Loader_Autoloader::getInstance();

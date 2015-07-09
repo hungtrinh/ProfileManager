2fec6d445ce11ecabd1bdeed4c952843504f92e2
Hung Trinh <trinhduchung@gmail.com>
2015-07-09 22:48:36 +0700

    Quick and dirty add code production to pass test when visit show profiles table

--- tests/application/controllers/ProfileListPageIntegrateDbTest.php
+++ tests/application/controllers/ProfileListPageIntegrateDbTest.php
@@ -42,5 +42,16 @@ class ProfileListPageIntegrateDbTest extends ControllerIntegrateDbTestCase
         $this->assertQueryContentContains('#table-list-profile-head th', 'fullname');
         $this->assertQueryContentContains('#table-list-profile-head th', 'age');
         $this->assertQueryContentContains('#table-list-profile-head th', 'email');
+
+        foreach ([
+            ['id' => 1, 'fullname' => 'Trinh An An', 'age' => 26, 'email' => 'an@gmail.com'],
+            ['id' => 2, 'fullname' => 'Trinh An Binh', 'age' => 25, 'email' => 'binh@gmail.com'],
+            ['id' => 3, 'fullname' => 'Trinh An Thai', 'age' => 24, 'email' => 'thai@gmail.com'],
+        ] as $profile) {
+            $this->assertQueryContentContains('#table-list-profile-body th', $profile['id']);
+            $this->assertQueryContentContains('#table-list-profile-body th', $profile['fullname']);
+            $this->assertQueryContentContains('#table-list-profile-body th', $profile['age']);
+            $this->assertQueryContentContains('#table-list-profile-body th', $profile['email']);
+        }
     }
 }

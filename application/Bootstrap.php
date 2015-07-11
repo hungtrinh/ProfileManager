ba32727cc9e05de783263c70cb26942d1065a9fd
Hung Trinh <trinhduchung@gmail.com>
2015-07-11 13:25:41 +0700

    Specification paginator api for ProfileRepositoryInterface AND discovery first api for profile mapper interface

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,7 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initLoaderResources()
+    protected function _initResourceLoader()
     {
         $this->getResourceLoader()->addResourceType('repository', 'repositories/', 'Repository');
     }
---
+++ tests/application/models/repositories/ProfileDiscoveryMapperApiTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Discovery api of lower layer using by Application_Repository_Profile
+ * 
+ * @group discovery-mapper-api
+ */
+class Application_Repository_ProfileDiscoveryMapperApiTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @var Application_Repository_Profile 
+     */
+    private $profileRepo;
+
+    /**
+     *
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $profileMapperMock;
+
+    protected function setUp()
+    {
+        $this->autoloadResource();
+        $this->profileMapperMock = $this->getMockBuilder('Application_Model_ProfileMapperInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->profileRepo       = new Application_Repository_Profile($this->profileMapperMock);
+    }
+
+    protected function autoloadResource()
+    {
+        $app = new Zend_Application(APPLICATION_ENV,
+            APPLICATION_PATH."/configs/application.ini");
+        /* 'ResourceLoader' get from '_initResourceLoader' in Boostrap::_initResourceLoader() */
+        $app->bootstrap('ResourceLoader');
+    }
+
+    /**
+     * @test
+     */
+    public function discoveryMapperPaginatorInterface()
+    {
+        $profileCollection = new Application_Model_ProfileCollection([new Application_Model_Profile()]);
+        $page              = 1;
+        $pageSize          = 25;
+
+        $this->profileMapperMock
+            ->expects($this->once())
+            ->method('paginator')
+            ->with($page, $pageSize)
+            ->will($this->returnValue($profileCollection));
+
+        $result = $this->profileRepo->paginator($page, $pageSize);
+        $this->assertSame($profileCollection, $result);
+    }
+}

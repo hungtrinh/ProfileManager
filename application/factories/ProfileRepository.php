afcbefdda8471667f2819077bc9b4b125245cb28
Hung Trinh <trinhduchung@gmail.com>
2015-07-17 10:27:12 +0700

    DONE code production for specification factory repository profile  will return mapped object if exists

--- application/factories/ProfileRepository.php
+++ application/factories/ProfileRepository.php
@@ -6,10 +6,15 @@
  */
 class Application_Factory_ProfileRepository
 {
+
     public function createService()
     {
-        return new Application_Repository_Profile(
-            new Application_Model_DbTable_Profile()
-        );
+        if (!Zend_Registry::isRegistered('Application_Repository_ProfileInterface')) {
+            $profileRepo = new Application_Repository_Profile(
+                new Application_Model_DbTable_Profile()
+            );
+            Zend_Registry::set('Application_Repository_ProfileInterface', $profileRepo);
+        }
+        return Zend_Registry::get('Application_Repository_ProfileInterface');
     }
 }
